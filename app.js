0738025898d70cd577fed906e99d27e2caa7aa9f
selcuklu <yilmax4@gmail.com>
2023-02-12 13:45:04 +0300

    nodemon was activated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get("/",(req,res)=>{
-    res.send("burasi home olacak")
+    res.send("burasi home olacak nodemon çalisti mi???")
 })
 
 app.listen(port,()=>{
