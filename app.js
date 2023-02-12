966843e4d226ac42f56665ee33d9d5657d2c657b
selcuklu <yilmax4@gmail.com>
2023-02-12 19:20:34 +0300

    index and about  ejs was setted

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ import express from "express"
 
 const app = express()
 const port = 3000
+// ejs template engine
+app.set("view engine","ejs")
+
+
+//static files middleware
+app.use(express.static("public"))
 
 app.get("/",(req,res)=>{
-    res.send("burasi home olacak nodemon çalisti mi???")
+    res.render("index")
+})
+app.get("/about",(req,res)=>{
+    res.render("about")
 })
 
 app.listen(port,()=>{
