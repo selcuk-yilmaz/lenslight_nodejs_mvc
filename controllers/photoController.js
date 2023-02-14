c0b0d34030c9cce70ae8c6c31153c5d399ea6e9f
selcuklu <yilmax4@gmail.com>
2023-02-14 09:04:57 +0300

    diynamic photo render

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -9,6 +9,24 @@ const createPhoto=async (req,res)=>{
         photo,
     })
 }
+const getAllPhotos = async (req,res)=>{
+    try {
+        const photos = await Photo.find({});
+        res.status(200).render("photos",{
+            photos,
+            link:"photos"
+        });
+        // .json({
+        //   succeded: true,
+        //   photos,
+        // });
+        
+    } catch (error) {
+        res.status(500).json({
+          succeded: true,
+          error,
+        });
+    }
+}
 
-
-export { createPhoto };
+export { createPhoto, getAllPhotos };
