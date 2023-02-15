5e916af83eee387fb4133746e13b591e45acf175
selcuklu <yilmax4@gmail.com>
2023-02-15 08:34:57 +0300

    singular photo was setted

--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -8,6 +8,6 @@ const router = express.Router();
 
 router.route("/").post(photoController.createPhoto);
 router.route("/").get(photoController.getAllPhotos);
-
+router.route("/:id").get(photoController.getAPhoto);
 
 export default router;
