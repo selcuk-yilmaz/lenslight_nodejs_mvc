2440ac931ecec3956f68ccb227b9ea56dcce56a3
selcuklu <yilmax4@gmail.com>
2023-03-14 23:05:55 +0300

    emai progress complated

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -13,6 +13,7 @@ router.route("/register").get(pageController.getRegisterPage)
 router.route("/login").get(pageController.getLoginPage)
 router.route("/logout").get(pageController.getLogout)
 router.route("/contact").get(pageController.getContactPage)
+router.route("/contact").post(pageController.sendMail)
 
 
 
